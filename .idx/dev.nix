8633e8d8628d765db2e597d00f1c4fdb7c7a58a8
eydamson <eydamson@gmail.com>
2024-03-27 00:12:04 +0000

    setup google idx dev preview

---
+++ .idx/dev.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    SOME_ENV_VAR = "hello";
+  };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    
+  ];
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+    previews = [
+      {
+        command = [
+          "npm"
+          "run"
+          "dev"
+        ];
+        manager = "web";
+        id = "web";
+      }
+    ];
+  };
+}
