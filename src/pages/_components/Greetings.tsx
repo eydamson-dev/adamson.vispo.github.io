3f3628310c6460f0b69bfa6f25b9e41976e75e6d
Adamson Vispo <eydamson.dev@gmail.com>
2024-06-17 00:10:14 +0800

    Adjust font size on mobile

--- src/pages/_components/Greetings.tsx
+++ src/pages/_components/Greetings.tsx
@@ -1,7 +1,7 @@
 export default function Greetings() {
   return (
-    <section className="hero-section min-h-svh grid snap-start grid-rows-1 place-items-center text-center leading-snug tracking-wide md:text-left">
-      <h1 className="font-dm-sans text-5xl font-medium">
+    <section className="hero-section min-h-svh grid w-full snap-start grid-rows-1 place-items-center text-center leading-snug tracking-wide @container md:text-left">
+      <h1 className="font-dm-sans text-2xl font-medium @lg:text-5xl">
         Hey there! I'm <span className="text-red-500">Adam</span>, a web developer with 8 years of experience. Passionate about coding, problem-solving, and empowering teams. Let's create something amazing!
       </h1>
       <a href="#my-projects-section" className="scroll-to">
