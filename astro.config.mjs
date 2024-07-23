1ded7997cd51ffe24907bd2815c4f2b8a76b25c4
Adamson Vispo <eydamson.dev@gmail.com>
2024-07-23 22:02:03 +0800

    Update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eydamson-dev.github.io',
-  base: '/',
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false
