ebffb24ce39b76dc687a34a3faa4cbaead207df2
Adamson Vispo <eydamson.dev@gmail.com>
2024-07-27 16:18:13 +0800

    update the greeting text

--- src/pages/_components/Greetings.tsx
+++ src/pages/_components/Greetings.tsx
@@ -8,7 +8,7 @@ export default function Greetings() {
     <section className="hero-section min-h-svh grid w-full snap-start grid-rows-1 place-items-center text-center tracking-wide @container md:text-left">
       <div className="grid justify-items-center gap-[24px] @2xl:grid-flow-col">
         <h1 className="text-center font-dm-sans text-3xl font-medium leading-snug @3xl:text-4xl @3xl:leading-snug @4xl:text-5xl @4xl:leading-snug">
-          Hey there! I'm <span className="text-red-500">Adam</span>, a web developer with 8 years of experience. Passionate about coding, problem-solving, and empowering teams. Let's create something amazing!
+          Hey there! I’m <span className='text-red-500'>Adam</span>. With 8 years in web development, including game dev, React, Next.js, and REST APIs, I thrive on creating efficient solutions, managing sprints, and supporting full-stack development.
         </h1>
         <div className={classNames(styles.cvContainer, "max-h-96 rounded-2xl")}>
           <a href={`${baseUrl}files/adamson-vispo-cv.pdf`} target='_blank'>
